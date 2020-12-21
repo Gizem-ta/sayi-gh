804a728b02e2912374fb527d2da730a88fb26751
Gizem-ta <76428363+Gizem-ta@users.noreply.github.com>
2020-12-21 14:38:23 +0300

    Add files via upload

---
+++ sayi gh.cpp
@@ -0,0 +1,38 @@
+#include <stdio.h>
+#include <math.h>
+int main()
+{
+int	kontrol =0, yenisayi;{
+for(int = 1;  i<=2000000;i++)
+{
+	int esay=0;
+	while(i>1) {
+		esay = esay+1;
+	}	if(i%2==0)
+		{
+			i=i/2; {
+			else{
+				i= 3*i+1;
+				{
+					esay =esay+1
+					if(esay>kontrol)
+					konrtol=esay;
+					yeni sayi= i;
+					
+					printf("ilk iki mýlyon sayi icinde,\n");
+					printf ("en fazla eleman sayisina sahip sayi= %d\n" yeni sayi);
+					printf("eleman sayisi",konrtol);
+					
+				}
+				
+					
+				}
+			}
+			
+			}
+		return 0;	
+		}
+	}
+}
+}
+
